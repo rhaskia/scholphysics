540356b6903570ee6c8eb7ef0e95ba19c055d033
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-22 20:23:24 +1300

    removed unnecessary tabs questiongen.js

--- src/Utils/questiongen.js
+++ src/Utils/questiongen.js
@@ -2,22 +2,22 @@ import { Question } from "../Components/Quiz";
 import data from '../../data/example.json' assert { type: 'json' };
 
 export function loadTemplate(path) {
-    return data;
+  return data;
 }
 
 export function generateQuestion(template) {
-    let values = template.valueRanges.map(r => randRange(r[0], r[1]));
-    console.log();
-    let question = template.question;
-    // TODO: generate answers
-    let answers = ["1", "2", "3", "4"];
-    let correctIdx = 2;
+  let values = template.valueRanges.map(r => randRange(r[0], r[1]));
+  console.log();
+  let question = template.question;
+  // TODO: generate answers
+  let answers = ["1", "2", "3", "4"];
+  let correctIdx = 2;
 
-    for (let i = 0; i < values.length; i++) {
-        question = question.replace("{" + i + "}", expo(values[i], 3));
-    } 
+  for (let i = 0; i < values.length; i++) {
+      question = question.replace("{" + i + "}", expo(values[i], 3));
+  } 
 
-    return new Question(template.title, question, answers, correctIdx);
+  return new Question(template.title, question, answers, correctIdx);
 }
 
 function randRange(min, max) {
