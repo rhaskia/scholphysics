71f516a297421f23be56a6ed1878f81e1d39d801
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-02-03 15:40:42 +1300

    routing halfway done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,28 @@
+import { useNavigate } from "react-router";
+
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  function routeToQuiz(path) {
+    navigate("waves");
+  }
+
   return (
     <div className="home-content flex-dir-column flex-sa-c">
       <h1 className="rubik-semibold">What would you like to study today?</h1>
 
       <ul className="quizzes-container flex-dir-row flex-sa-c">
-        <li className="flex-dir-column flex-c-c">
+        <li className="flex-dir-column flex-c-c" onClick={routeToQuiz}>
           <h4 className="lato">Waves</h4>
           <h4>🌊</h4>
         </li>
 
-        <li className="flex-dir-column flex-c-c">
+        <li className="flex-dir-column flex-c-c" onClick={routeToQuiz}>
           <h4 className="lato">Mechanics</h4>
           <h4>🤖</h4>
         </li>
 
-        <li className="flex-dir-column flex-c-c">
+        <li className="flex-dir-column flex-c-c" onClick={routeToQuiz}>
           <h4 className="lato">Electricity</h4>
           <h4>💡</h4>
         </li>
