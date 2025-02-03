56421863cca9fb9698668edd6f1edefa284ede65
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-02-03 15:28:47 +1300

    done rough template

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,3 +1,14 @@
 export const HomePage = () => {
-  return <div>Hi!</div>;
+  return (
+    <div className="home-content flex-dir-column">
+      <h1 className="rubik-semibold">What would you like to study today?</h1>
+
+      <div className="quizzes-container flex-dir-row">
+        <li className="flex-dir-column flex-c-c">
+          <h4>Waves</h4>
+          <h4>🌊</h4>
+        </li>
+      </div>
+    </div>
+  );
 };
