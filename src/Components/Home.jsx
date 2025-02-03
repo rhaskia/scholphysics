551a71e8438a3f86e283f869d3d30aa10f1f80df
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-02-03 15:35:30 +1300

    done home page layout

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,26 @@
 export const HomePage = () => {
   return (
-    <div className="home-content flex-dir-column">
+    <div className="home-content flex-dir-column flex-sa-c">
       <h1 className="rubik-semibold">What would you like to study today?</h1>
 
-      <div className="quizzes-container flex-dir-row">
+      <ul className="quizzes-container flex-dir-row flex-sa-c">
         <li className="flex-dir-column flex-c-c">
-          <h4>Waves</h4>
+          <h4 className="lato">Waves</h4>
           <h4>🌊</h4>
         </li>
-      </div>
+
+        <li className="flex-dir-column flex-c-c">
+          <h4 className="lato">Mechanics</h4>
+          <h4>🤖</h4>
+        </li>
+
+        <li className="flex-dir-column flex-c-c">
+          <h4 className="lato">Electricity</h4>
+          <h4>💡</h4>
+        </li>
+      </ul>
+
+      <div>{/* Empty div to make quiz selection buttons centered*/}</div>
     </div>
   );
 };
